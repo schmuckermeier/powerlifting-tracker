32e51b54a02bf971548f677c4fdf7a75785c7529
Sophie <schmuckermeier@googlemail.com>
2021-09-10 17:02:16 +0200

    create weight/reps input form

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -23,16 +23,40 @@ const App = () => {
   }
 
   return (
-    <div className="container">
+    <div className="container mt-5">
+      <div className="row mb-5">
+        <h1>Powerlifting Tracker</h1>
+      </div>
+      <form>
+
+        <div className="form-group row mb-5">
+          <label htmlFor="exerciseSelect" className="col-sm-2 col-form-label">Exercise</label>
+          <div className="col-sm-10">
+            <select className="form-control form-control-lg col-sm-10" id="exerciseSelect">
+              <option>Squat</option>
+              <option>Bench</option>
+              <option>Deadlift</option>
+              <option>Shoulder Press</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="form-group row mb-5">
+          <label htmlFor="weight" className="col-2 col-form-label">Weight</label>
+          <div className="col-4">
+            <input type="number" className="form-control" id="weight" placeholder="10kg"/>
+          </div>
+
+          <label htmlFor="reps" className="col-2 col-form-label">Reps</label>
+          <div className="col-4">
+            <input type="number" className="form-control" id="reps" placeholder="5"/>
+          </div>
+        </div>
+      </form>
+
       <div className="row">
-        <h1>Todos</h1>
-        <ul className="list-group mb-5">
-          {todos.map((t) => (
-            <li className={t.active ? 'list-group-item active' : 'list-group-item'} key={t.title}>{t.title}</li>
-          ))}
-        </ul>
-        <button id={'add-todo'} onClick={addTodo} className="btn btn-primary">
-          Add Todo
+        <button onClick={addTodo} className="btn btn-primary col-6 offset-3">
+          Add
         </button>
       </div>
     </div>
