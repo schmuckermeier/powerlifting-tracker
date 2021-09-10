5fc2df9d18a876fd33475bb14b1cd720108d6c8b
Sophie <schmuckermeier@googlemail.com>
2021-09-10 15:23:50 +0200

    initial commit

--- apps/frontend/jest.config.js
+++ apps/frontend/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  displayName: 'powerlifting-tracker',
+  displayName: 'frontend',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/powerlifting-tracker',
+  coverageDirectory: '../../coverage/apps/frontend',
 };
